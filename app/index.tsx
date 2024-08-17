47e118c8d254fb3b664ba8404a5371b79c5fb19c
RoyGalaxy <ajroysstyle@gmail.com>
2024-08-17 14:33:41 +0530

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View, SafeAreaView } from "react-native";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
